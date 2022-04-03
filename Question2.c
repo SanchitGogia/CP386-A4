27fc2ddaff5ccda7715d250d5b1d5e8057d6fca5
nick <nicholassam6425@gmail.com>
2022-04-03 06:04:06 -0400

    Create Question2.c
    basic input stuff for now, struct for processes to hold name and memory size? maybe change later idk

---
+++ Question2.c
@@ -0,0 +1,24 @@
+/*
+nicholassam6425 190148430
+SanchitGogia
+q2
+
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+void main(int argc, char *argv[])
+{
+    struct process {
+        int memory;
+        char name[8];
+    };
+    struct process plist[20];
+    int freelist[20];
+    char input[40] = "";
+    int memory_size = strtol(argv[1], NULL, 10);
+    while(strcmp("Exit", input) ) {
+        
+    }
+}
