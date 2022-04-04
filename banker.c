f64ad08a2e544909b520c32f32b71878d5b2e740
Tlou <frl.sanchit@gmail.com>
2022-04-03 23:01:58 -0400

    banker algo with safe sequence check

--- banker.c
+++ banker.c
@@ -33,8 +33,54 @@ void display(int maximum[][4],int allocated[][4], int need[][4], int available[]
 
     
 }
+int safeSequence(int allocated[][4], int need[][4], int available[]){
+    //int need1[5][4] = need;
+    int available1[4]; 
+    memcpy(available1,available,4*sizeof(int));
+    int completion[5] = {0,0,0,0,0};
+    //int safeSequence[5] = {-1,-1,-1,-1,-1};
+    //int failState[1] = {-1};
+    int c_count = 0; //count of completed customers for the sake of safe sequence
+    int i = 0;
+    while (c_count<5){
+        printf("%d: \n",i);
+        if (i == 5 && c_count<5) return 0;
+        if (completion[i] == 0){
+            int flag = 1;
+            for(int x = 0; x<4;x++){
+                if(need[i][x]>available1[x]){
+                    printf("%d, %d, (%d , %d)\n",i,x, need[i][x],available1[x]);
+                    flag = 0;
+                    break;
+                }
+            }
+            if(flag == 1){
+                for (int x=0; x<4;x++){
+                    available1[x] = available1[x] + allocated[i][x];
+                    //printf("BITCH");
+
+                }
+
+                completion[i] = 1;
+                printf("--------------");
+                c_count++;
+                i = 0;
+                
+            }
+            else i++;
+            
+        }
+        else i++;
+        
+        
+    }
+    return 1;
+}
+
+
 int main(int argc, char *argv[]){
     setbuf(stdout,NULL);
+    
     if (argc != 5){
         printf("Invalid input, try again\n");
         return 1;
@@ -67,7 +113,7 @@ int main(int argc, char *argv[]){
         fgets(a,50,stdin);
         a[strcspn(a, "\n")] = 0;
         //a[strlen(a) - 1] = "\0";
-        printf("%s\n",a);
+        //printf("%s\n",a);
         char *p = strtok(a," ");
         if(strcmp(p,"RQ")==0) {
             printf("%s\n",p);
@@ -77,7 +123,7 @@ int main(int argc, char *argv[]){
             while(p!= NULL){
                 
                 temp[i] = atoi(p);
-                printf("%d ,",temp[i]);
+                //printf("%d ,",temp[i]);
                 i++;
                 p = strtok(NULL, " ");  
             }
@@ -87,16 +133,32 @@ int main(int argc, char *argv[]){
             if(temp[4] == -1) printf("Invalid input, use one of RQ, RL, Status, Run, Exit \n");
             else{
                 if((need[temp[0]][0] - temp[1]>=0 && available[0] - temp[1] >=0 )&&(need[temp[0]][1] - temp[2]>=0 && available[1] - temp[2] >=0 )&&(need[temp[0]][2] - temp[3]>=0 && available[2] - temp[3] >=0 )&&
-                (need[temp[0]][0] - temp[1]>=0 && available[0] - temp[1] >=0 )){
+                (need[temp[0]][3] - temp[4]>=0 && available[3] - temp[4] >=0 )){
                     while (i < 4){
                         available[i] = available[i] - temp[i+1];
                         need[temp[0]][i] = need[temp[0]][i] - temp[i+1];
                         allocated[temp[0]][i] = allocated[temp[0]][i] + temp[i+1];
-                        temp[i+1] = -1;  
+                        //temp[i+1] = -1;  
                         i++;
                     }
-                    temp[0] = -1;
-                    printf("State is safe, and request is satisfied\n");
+                    //temp[0] = -1;
+                    int v = safeSequence(allocated,need,available);
+                    printf("SS = %d",v);
+                    if( v== 1){
+                        for (int x = 0; x< 5;x++) temp[x] = -1;
+                        printf("State is safe, and request is satisfied\n");}
+                    else {
+                        i = 0;
+                        while (i < 4){
+                            available[i] = available[i] + temp[i+1];
+                            need[temp[0]][i] = need[temp[0]][i] + temp[i+1];
+                            allocated[temp[0]][i] = allocated[temp[0]][i] - temp[i+1];
+                            temp[i+1] = -1;  
+                            i++;
+                    }
+                        temp[0] = -1;
+                        printf("State unsafe, request denied\n");
+                    }
                     /*available[0] = available[0] - temp[1];
                     available[1] = available[0] - temp[2];
                     available[2] = available[0] - temp[3];
